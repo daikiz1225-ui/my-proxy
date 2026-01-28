be015d328eab199e696601f4858ba80623d18562
daikiz1225-ui <daikiz1225@icloud.com>
2026-01-28 12:08:28 +0900

    Create headers.js

---
+++ api/headers.js
@@ -0,0 +1,6 @@
+export default function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-YouTube-Client-Name, X-YouTube-Client-Version');
+    res.status(200).end();
+}
