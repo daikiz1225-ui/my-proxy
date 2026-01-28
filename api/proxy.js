c98e3bd1c32dea0f99809c21d7f6be2b1d9ba856
daikiz1225-ui <daikiz1225@icloud.com>
2026-01-28 11:41:02 +0900

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -9,19 +9,18 @@ export default async function handler(req, res) {
             headers: {
                 'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 17_0 like Mac OS X) AppleWebKit/605.1.15',
                 'Accept': '*/*',
-                'Accept-Language': 'ja-JP,ja;q=0.9',
-                'Referer': new URL(decodedUrl).origin
+                'Referer': new URL(decodedUrl).origin,
+                'Cookie': req.headers.cookie || ''
             }
         });
 
-        // 相手のContent-Typeをそのまま引き継ぐ
-        const contentType = response.headers.get('content-type');
-        res.setHeader('Content-Type', contentType || 'text/html');
+        res.setHeader('Content-Type', response.headers.get('content-type') || 'text/html');
         res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Credentials', 'true');
 
         const buffer = await response.arrayBuffer();
         res.send(Buffer.from(buffer));
     } catch (e) {
-        res.status(500).send("Proxy Error");
+        res.status(500).send("Error");
     }
 }
