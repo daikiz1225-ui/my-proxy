5f8c502d26d526b63e1cbd8c244997bcbb18802f
daikiz1225-ui <daikiz1225@icloud.com>
2026-01-29 02:47:04 +0900

    Delete api/adblock.js

--- api/adblock.js
+++
@@ -1,7 +0,0 @@
-function clean(html) {
-    let cleanHtml = html.replace(/<script[^>]*googlesyndication[^>]*><\/script>/gi, '');
-    cleanHtml = cleanHtml.replace(/<script[^>]*google-analytics[^>]*><\/script>/gi, '');
-    const adHideStyle = `<style>ins.adsbygoogle, .ad-slot, .ad-container, div[id*="google_ads"], iframe[src*="google"] { display: none !important; }</style>`;
-    return cleanHtml.replace('</head>', adHideStyle + '</head>');
-}
-module.exports = { clean };
