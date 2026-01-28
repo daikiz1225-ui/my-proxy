f797d4d4553c246128a31fd46295e5cf11b8ec8e
daikiz1225-ui <daikiz1225@icloud.com>
2026-01-29 01:40:19 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 self.addEventListener('fetch', (event) => {
     const url = new URL(event.request.url);
     if (url.host === location.host) return;
+
+    // 完全に全ての通信を隠蔽
     const b64 = btoa(unescape(encodeURIComponent(event.request.url))).replace(/\//g, '_').replace(/\+/g, '-');
     event.respondWith(fetch('/api/proxy?url=' + b64));
 });
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => e.waitUntil(clients.claim()));
