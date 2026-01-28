6859763693ecf70165519b7d6247c46b818d643d
daikiz1225-ui <daikiz1225@icloud.com>
2026-01-29 00:46:11 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,11 +2,17 @@ self.addEventListener('fetch', (event) => {
     const url = event.request.url;
     if (url.includes(location.host) || !url.startsWith('http')) return;
 
+    // リンク移動、画像、API通信、すべてを個別のプロキシに振り分ける
     const b64 = btoa(unescape(encodeURIComponent(url))).replace(/\//g, '_').replace(/\+/g, '-');
     
+    let proxyPath = '/api/proxy';
+    if (url.includes('youtube.com')) proxyPath = '/api/youtube';
+    if (url.includes('poki.com')) proxyPath = '/api/poki';
+
     event.respondWith(
-        fetch('/proxy/' + b64).then(response => {
-            return response;
+        fetch(`${proxyPath}?url=${b64}`, {
+            headers: event.request.headers,
+            credentials: 'include'
         }).catch(() => fetch(event.request))
     );
 });
