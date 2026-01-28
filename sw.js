14af4e30621bf9b94eba3f7ed35fad88f850c837
daikiz1225-ui <daikiz1225@icloud.com>
2026-01-28 12:02:02 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,19 @@
-self.addEventListener('install', (e) => self.skipWaiting());
-self.addEventListener('activate', (e) => e.waitUntil(clients.claim()));
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(clients.claim()));
 
 self.addEventListener('fetch', (event) => {
     const url = event.request.url;
     if (url.includes(location.host) || !url.startsWith('http')) return;
 
-    // 通信をBase64で包んで、i-FILTERの検閲をスルーさせる
     const b64 = btoa(unescape(encodeURIComponent(url))).replace(/\//g, '_').replace(/\+/g, '-');
     
     event.respondWith(
         fetch('/proxy/' + b64, {
-            method: event.request.method,
             headers: event.request.headers,
-            credentials: 'include' // これがオフライン対策に重要
+            credentials: 'include'
+        }).then(res => {
+            // CSSが巨大化しないよう、Content-Typeを正しく維持する
+            return res;
         }).catch(() => fetch(event.request))
     );
 });
