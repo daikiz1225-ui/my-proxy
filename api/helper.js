6885d29bc618db8d47325ef251b82fbe3ea4d906
daikiz1225-ui <daikiz1225@icloud.com>
2026-01-29 00:28:25 +0900

    Update helper.js

--- api/helper.js
+++ api/helper.js
@@ -1,3 +1,4 @@
 export default function handler(req, res) {
-    res.status(200).send("Ready to inject");
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.json({ status: "Helper Active", mode: "Bypass" });
 }
