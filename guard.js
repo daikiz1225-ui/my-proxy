29a6c9980971d96cfeb5ba24a03c9c585dcd5e22
daikiz1225-ui <daikiz1225@icloud.com>
2026-01-29 01:26:30 +0900

    Update guard.js

--- guard.js
+++ guard.js
@@ -1,13 +1,9 @@
-// guard.js
 (function() {
-    const ironCladOnline = () => {
+    const forceOnline = () => {
         Object.defineProperty(navigator, 'onLine', { get: () => true, configurable: true });
         window.dispatchEvent(new Event('online'));
     };
-    ironCladOnline();
-    setInterval(ironCladOnline, 100); // 0.1秒ごとにオンラインを強制
-
-    // YouTube用の言語設定を焼き付ける
-    document.cookie = "PREF=hl=ja&gl=JP; domain=.youtube.com; path=/";
+    forceOnline();
+    setInterval(forceOnline, 500); // 0.5秒ごとにチェック
     console.log("Specialist: Guard active.");
 })();
