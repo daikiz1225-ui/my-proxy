3912247aea9326e71a542f625cbb739876bacb60
daikiz1225-ui <daikiz1225@icloud.com>
2026-01-28 12:50:27 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,8 @@
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((names) => {
-            for (let name of names) caches.delete(name);
-        }).then(() => self.clients.claim())
-    );
-});
-
 self.addEventListener('fetch', (event) => {
-    // 自分のサイト以外へのリクエストは全てBase64プロキシへ
-    if (!event.request.url.includes(location.host)) {
+    const url = new URL(event.request.url);
+    
+    // 自分のサーバーへのリクエスト以外をすべて捕まえる
+    if (!url.host.includes(location.host)) {
         const b64 = btoa(unescape(encodeURIComponent(event.request.url))).replace(/\//g, '_').replace(/\+/g, '-');
         event.respondWith(fetch('/proxy/' + b64));
     }
