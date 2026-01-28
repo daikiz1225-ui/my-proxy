27f8cbbb62cd27533c40684d8a627520f0f4f56c
daikiz1225-ui <daikiz1225@icloud.com>
2026-01-29 01:25:21 +0900

    Delete lang.js

--- lang.js
+++
@@ -1,9 +0,0 @@
-// YouTubeの内部変数を書き換えて日本語を強制する
-window.yt = window.yt || {};
-window.ytcfg = window.ytcfg || {};
-window.ytcfg.set = window.ytcfg.set || function() {};
-document.cookie = "PREF=hl=ja&gl=JP; domain=.youtube.com; path=/";
-document.documentElement.lang = "ja";
-
-// i-FILTERの「URLチェック」を回避するために履歴を偽装
-history.replaceState(null, '', '/study-mode');
